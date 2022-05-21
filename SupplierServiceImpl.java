5b69eff9499eaeed00d873c41376e224a66d2278
a淞 <100818579+songIsland@users.noreply.github.com>
2022-05-21 23:55:44 +0800

    service层实现SupplierService接口的类

---
+++ SupplierServiceImpl.java
@@ -0,0 +1,52 @@
+package com.system.service;
+
+import java.util.List;
+
+import com.system.pojo.Supplier;
+
+public class SupplierServiceImpl implements SupplierService{
+
+
+
+	
+
+	@Override
+	public int addSupplier(Supplier supplier) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static Object getBySupplierCreditCode(String creditCodeNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delDetailsByNo(String creditCode) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getDetailsByNo(String creditCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int editDetailsByNo(Supplier supplier) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Supplier> getDetailsByBarCode(String creditCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+}
