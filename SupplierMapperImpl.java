850547d859d909fa96f2f09d2c40b210981ed2cb
a淞 <100818579+songIsland@users.noreply.github.com>
2022-05-27 21:12:45 +0800

    Supplier模块Mapper.impl层函数方法实现

--- SupplierMapperImpl.java
+++ SupplierMapperImpl.java
@@ -1,57 +1,87 @@
-package com.system.mapper;
+package com.system.mapper.impl;
 
 import java.util.List;
 
-import com.system.pojo.Supplier;
+import org.apache.ibatis.session.SqlSession;
 
-public class SupplierMapperImpl implements SupplierMapper{
+import com.system.mapper.SupplierMapper;
+import com.system.pojo.Supplier;
+import com.system.util.MybatisUtil;
+
+public  class SupplierMapperImpl implements SupplierMapper{
 
 	@Override
 	public int addSupplier(Supplier supplier) {
-		// TODO Auto-generated method stub
-		return 0;
+		SqlSession session = null;
+		session = MybatisUtil.createSqlSession();
+		int count = session.getMapper(SupplierMapper.class).addSupplier(supplier);
+		session.commit();
+		if(session != null)
+			session.close();
+		return count;
 	}
 
 	@Override
 	public int delSupplierByNo(String creditCode) {
-		// TODO Auto-generated method stub
-		return 0;
+		SqlSession session = null;
+		session = MybatisUtil.createSqlSession();
+		int count = session.getMapper(SupplierMapper.class).delSupplierByNo(creditCode);
+		session.commit();
+		if(session != null)
+			session.close();
+		return count;
 	}
 
 	@Override
 	public int editSupplierByNo(Supplier supplier) {
-		// TODO Auto-generated method stub
-		return 0;
+		SqlSession session = null;
+		session = MybatisUtil.createSqlSession();
+		int count = session.getMapper(SupplierMapper.class).editSupplierByNo(supplier);
+		session.commit();
+		if(session != null)
+			session.close();
+		return count;
 	}
 
 	@Override
 	public List<Supplier> getSupplierByCreditCode(String creditCode) {
-		// TODO Auto-generated method stub
-		return null;
+		SqlSession session = null;
+		session = MybatisUtil.createSqlSession();
+		List<Supplier> suppliers= session.getMapper(SupplierMapper.class).getSupplierByCreditCode(creditCode);
+		if(session != null)
+			session.close();
+		return suppliers;
 	}
 
 	@Override
 	public List<Supplier> getSupplierByName(String s_name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getSupplierCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<Supplier> getPartAllSupplier(int currentIndex, int size) {
-		// TODO Auto-generated method stub
-		return null;
+		SqlSession session = null;
+		session = MybatisUtil.createSqlSession();
+		List<Supplier> suppliers = session.getMapper(SupplierMapper.class).getSupplierByName(s_name);
+		if(session != null)
+			session.close();
+		return suppliers;
 	}
 
 	@Override
 	public List<Supplier> getSupplierList() {
-		// TODO Auto-generated method stub
-		return null;
+		SqlSession session = null;
+		session = MybatisUtil.createSqlSession();
+		List<Supplier> suppliers = session.getMapper(SupplierMapper.class).getSupplierList();
+		if(session != null)
+			session.close();
+		return suppliers;
 	}
 
+	@Override
+	public int getSupplierCount() {
+		SqlSession session = null;
+		session = MybatisUtil.createSqlSession();
+		int count = session.getMapper(SupplierMapper.class).getSupplierCount();
+		if(session != null)
+			session.close();
+		return count;
+	}
+
+	
 }
